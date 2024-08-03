2c1984a16ba2f83711efbb94c9a595dd6e442af5
eedu7 <mueedahmad067@gmail.com>
2024-08-03 21:35:05 +0500

    FEAT: created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-import {Register} from "./pages/Register";
-import {Login} from "./pages/Login";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+
 function App() {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Register />,
+        },
+        {
+            path: "/login",
+            element: <Login />
+        },
+        {
+            // Appointment
+        },
+        {
+            // Profile Information
+        },
+        {
+            // Dashboard
+        },
+        {
+            // Successfull Appointment
+        },
+
+    ]);
 
   return (
-    <>
-    <Register />
-    </>
+    <RouterProvider router={router} />
   );
 }
 
